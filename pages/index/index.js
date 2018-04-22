ea6571d27adbe9d479c53427f9f2f8e074092d5e
muxiaolei <1484542698@qq.com>
2018-04-23 00:22:11 +0800

    nothing

--- pages/index/index.js
+++ pages/index/index.js
@@ -87,7 +87,7 @@ Page({
         hasUserInfo: true
       })
       that.userLogin();
-    } else{
+    } else {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       consoleUtil.log(2);
