5aa30309ad11edacc335e650746bcdf4927fec4b
muxiaolei <1484542698@qq.com>
2018-04-25 19:10:03 +0800

    逻辑修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -34,7 +34,7 @@ Page({
     longitude: '',
     latitude: '',
     //地图缩放级别
-    scale: 16,
+    scale: 14,
     markers: [],
     showTopTip: false,
     warningText: '',
@@ -94,6 +94,8 @@ Page({
 
   onLoad: function (options) {
     var that = this;
+    //检测更新
+    that.checkUpdate();
     if (app.globalData.userInfo) {
       consoleUtil.log(1);
       this.setData({
@@ -1024,6 +1026,37 @@ Page({
     })
   },
 
+  /**
+   * 版本更新
+   */
+  checkUpdate: function () {
+    if (wx.canIUse('getUpdateManager')){
+      const updateManager = wx.getUpdateManager();
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        consoleUtil.log('onCheckForUpdate----------------->');
+        consoleUtil.log(res.hasUpdate);
+      })
+
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，即刻体验？',
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            }
+          }
+        })
+      })
+
+      updateManager.onUpdateFailed(function () {
+        // 新的版本下载失败
+      })
+    }
+  },
+  
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
